26a95c2ec48ace2245990057b4e5da9d22dce7cb
hwclass <harderworking.class@gmail.com>
2014-11-10 01:15:54 +0200

    Changed the route links on the navigation in the main view.

--- app/js/routes.js
+++ app/js/routes.js
@@ -2,8 +2,8 @@
 
 define(['app'], function (app) {
   return app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/html/main.html', controller: 'MyController1'});
-    $routeProvider.when('/list', {templateUrl: 'partials/html/list.html', controller: 'MyController2'});
+    $routeProvider.when('#/home', {templateUrl: 'partials/html/main.html', controller: 'MyController1'});
+    $routeProvider.when('#/list', {templateUrl: 'partials/html/list.html', controller: 'MyController2'});
     $routeProvider.otherwise({redirectTo: '/main'});
   }]);
 });
