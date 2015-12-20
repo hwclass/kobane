17fdf5ab8a50ce984342b02c0002662e632b83b2
hwclass <harderworking.class@gmail.com>
2015-12-21 00:29:00 +0200

    Changed the invoking object properties for locales as dot notation

--- app/js/controller/my_controller1.js
+++ app/js/controller/my_controller1.js
@@ -4,7 +4,7 @@
  */
 define(function () {
 	'use strict';
-  
+
   return ['$scope', 'LocaleService', 'Config1Service', '$firebase', function ($scope, LocaleService, Config1Service, $firebase) {
 
   	$scope.locale = Config1Service.getLocale(Config1Service.getLang());
@@ -14,10 +14,10 @@ define(function () {
   	$scope.lang = Config1Service.getLang();
 
   	$scope.setStringMessagesIntoCurrentLocale = function () {
-  		$scope.MSG_HELLO = $scope.locale['MSG_HELLO'];
-  		$scope.MSG_HOW_ARE_YOU = $scope.locale['MSG_HOW_ARE_YOU'];
-  		$scope.MSG_THANK_YOU = $scope.locale['MSG_THANK_YOU'];
-  		$scope.MSG_CONTENT = $scope.locale['MSG_CONTENT'];
+  		$scope.MSG_HELLO = $scope.locale.MSG_HELLO;
+  		$scope.MSG_HOW_ARE_YOU = $scope.locale.MSG_HOW_ARE_YOU;
+  		$scope.MSG_THANK_YOU = $scope.locale.MSG_THANK_YOU;
+  		$scope.MSG_CONTENT = $scope.locale.MSG_CONTENT;
   	}
 
   	$scope.changeLocale = function (lang) {
@@ -34,9 +34,9 @@ define(function () {
   	$scope.setStringMessagesIntoCurrentLocale();
 
     var ref = new Firebase(Config1Service.firebase.url);
-    
+
     var sync = $firebase(ref);
-    
+
     $scope.data = sync.$asObject();
 
     console.log($scope.data);
